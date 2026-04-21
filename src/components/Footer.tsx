e5a686eb1723e2dda2beafb35d6ef0605e6491ed
Luciano França Rocha <luciano@debian.local>
2026-04-21 16:04:05 -0300

    fix: Atualizado localização de contato para Vitória - ES

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -43,7 +43,7 @@ export default function Footer() {
             <ul className={styles.links}>
               <li><a href="mailto:luciano.rocha@lfrautomacao.eng.br">luciano.rocha@lfrautomacao.eng.br</a></li>
               <li><a href="https://www.linkedin.com/in/lucianofr/" target="_blank" rel="noopener noreferrer">LinkedIn</a></li>
-              <li>São Paulo, SP - Brasil</li>
+              <li>Vitória, ES - Brasil</li>
             </ul>
           </div>
         </div>
